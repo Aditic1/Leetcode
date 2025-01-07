5553dc5432ffec35fdb33ec8421a57d8ca766814
Aditic1 <110151007+Aditic1@users.noreply.github.com>
2025-01-07 10:46:30 +0530

    Time: 1 ms (59.41%), Space: 11.6 MB (33.76%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        
+        unordered_set<string> a;
+
+        for(int i = 0; i < words.size(); i++) {
+            for(int j = 0; j < words.size(); j++) {
+                if(i != j && words[j].find(words[i]) != string::npos) {
+                    a.insert(words[i]); 
+                }
+            }
+        }
+
+        // Convert set to vector
+        return vector<string>(a.begin(), a.end());
     }
 };
